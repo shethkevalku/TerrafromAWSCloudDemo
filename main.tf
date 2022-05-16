9df49f581a753c05eacec77972da1e8c849f9662
shethkevalku <105679652+shethkevalku@users.noreply.github.com>
2022-05-16 23:35:32 +0100

    Updating further changes

--- main.tf
+++ main.tf
@@ -1,32 +1,3 @@
-# Require TF version to be same as or greater than 0.12.13
-terraform {
-  required_version = ">=0.12.13"
-  #backend "s3" {
-  #  bucket         = "kyler-github-actions-demo-terraform-tfstate"
-  #  key            = "terraform.tfstate"
-  #  region         = "us-east-1"
-  #  dynamodb_table = "aws-locks"
-  #  encrypt        = true
-  #}
-}
-
-# Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
-provider "aws" {
-  region  = "us-east-1"
-  version = "~> 2.36.0"
-}
-
-# Call the seed_module to build our ADO seed info
-module "bootstrap" {
-  source                      = "./modules/bootstrap"
-  name_of_s3_bucket           = "kyler-github-actions-demo-terraform-tfstate"
-  dynamo_db_table_name        = "aws-locks"
-  iam_user_name               = "GitHubActionsIamUser"
-  ado_iam_role_name           = "GitHubActionsIamRole"
-  aws_iam_policy_permits_name = "GitHubActionsIamPolicyPermits"
-  aws_iam_policy_assume_name  = "GitHubActionsIamPolicyAssume"
-}
-
 # Build the VPC
 resource "aws_vpc" "vpc" {
   cidr_block           = "10.1.0.0/16"
